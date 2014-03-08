313f172750e1a3e67eaf511ffdf8f417dbf15412
akhilnambiar <akhilnambiar@berkeley.edu>
2014-03-08 14:25:16 -0800

    HEROK

--- applicationcontroller.js
+++ applicationcontroller.js
@@ -29,26 +29,8 @@ app.get('/', function(req, res) {
 app.post('/search', function(req, res) {
   res.writeHead(200);
   var query = req.body.q;
-  /*
-  function superCallBack(result) {
-    res.end(result);
-  }
-
-  search_controller.search(req.body.q,superCallBack);
-});
-*/
-var called = false;
-  while(typeof query != 'undefined' && called==false)
-  {
-    result = search_controller.search(query);
-    called=true;
-  }
-  while(typeof result != 'undefined')
-  {
-    console.log(result);
-    res.end(result);
-  }
-});
+  console.log(search_controller.search(2));
+  
   /*
     TODO: MOVE THIS TO THE SEARCHCONTROLLER
 
@@ -63,8 +45,85 @@ var called = false;
   */
   //req.get("/JON");
   //res.get("/CAMILLE");
+    jsonArray = []
+    recArray=[];
+    recnameArray=[];
+    siuArray=[];
+    dArray=[];
+    ilArray=[];
+    var credentials = {
+      id: '13944c3c',
+      key: '5a09042c7587234cbd1adc10150874cf'
+    }
+    yummly.search({
+      credentials: credentials,
+      query: {
+        q: query
+      }
+    }, function (error, response, json) {
+      if (error) {
+        console.error(error);
+      } else if (response.statusCode === 200) {
+        console.log("FRUITCAKES");
+        console.log(json.matches[0].id);
+        var new_son = {
+          errCode: UserModel.ERR_BAD_CREDENTIALS,
+          count: result.rows[0].count
+        };
+        for (var i=0;i<10;i++) {
+            recArray.push(json.matches[i].id);
+            recnameArray.push(json.matches[i].recipeName);
+            siuArray.push(json.matches[i].smallImageUrls);
+            dArray.push(json.matches[i].sourceDisplayName);
+            ilArray.push(json.matches[i].ingredients);
+        }
+        new_son = {
+        recipe_id : recArray, 
+        recipe_name: recnameArray, 
+        smallImageUrls:siuArray, 
+        details: dArray,
+        ingredient_list: ilArray
+        };
+        var format_son = JSON.stringify(new_son);
+        res.write(format_son);
+      }
+
+
+      /*
+      res.write(json.matches[0].recipeName);
+      res.write(json.matches[1].recipeName);
+      res.write(json.matches[2].recipeName);
+      res.write(json.matches[3].recipeName);
+      res.write(json.matches[4].recipeName);
+      res.write(json.matches[5].recipeName);
+      res.write(json.matches[6].recipeName);
+      res.write(json.matches[7].recipeName);
+      res.write(json.matches[8].recipeName);
+      res.write(json.matches[9].recipeName);
+      //res.end(json.matches[0].recipeName);
+      */
+      for (var i=0;i<10;i++) {
+            recArray.push(json.matches[i].id);
+            recnameArray.push(json.matches[i].recipeName);
+            siuArray.push(json.matches[i].smallImageUrls);
+            dArray.push(json.matches[i].sourceDisplayName);
+            ilArray.push(json.matches[i].ingredients);
+    }
+      
+      new_son = {
+        recipe_id : recArray, 
+        recipe_name: recnameArray, 
+        smallImageUrls:siuArray, 
+        details: dArray,
+        ingredient_list: ilArray
+        };
+      var format_son = JSON.stringify(new_son);
+      res.end(format_son);
+    });
     //res.write('<html><body>');
     //res.write('CHOCO TACO');
+    
+});
 
 
 app.post('/users/login', function(req, res) {
