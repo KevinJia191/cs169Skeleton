a6c038013b6893552d5f19b59d37a553537090fd
akhilnambiar <akhilnambiar@berkeley.edu>
2014-03-07 19:58:57 -0800

    lesdo

--- historycontroller.js
+++ historycontroller.js
@@ -3,10 +3,9 @@ var logfmt = require("logfmt");
 var app = express();
 var pg = require('pg');
 
-var user_controller = require('/usercontroller.js');
-var ingredient_controller = require('/ingredientcontroller.js');
-var history_controller = require('/historycontroller.js');
-var search_controller = require('/searchcontroller.js');
+var user_controller = require('./usercontroller.js');
+var ingredient_controller = require('./ingredientcontroller.js');
+var search_controller = require('./searchcontroller.js');
 
 //take in a "recipe" object eventually
 var make = function(user, recipeData){
