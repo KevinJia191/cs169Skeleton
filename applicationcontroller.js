4d584e09b19832f7b17b017bcc8c3546b9f613bd
akhilnambiar <akhilnambiar@berkeley.edu>
2014-03-08 10:44:03 -0800

    HEROK

--- applicationcontroller.js
+++ applicationcontroller.js
@@ -30,6 +30,8 @@ app.post('/search', function(req, res) {
   res.writeHead(200);
   var query = req.body.q;
   console.log(search_controller.search(2));
+  result = search_controller.search(query);
+  res.end(result);
   
   /*
     TODO: MOVE THIS TO THE SEARCHCONTROLLER
@@ -45,83 +47,6 @@ app.post('/search', function(req, res) {
   */
   //req.get("/JON");
   //res.get("/CAMILLE");
-    jsonArray = []
-    recArray=[];
-    recnameArray=[];
-    siuArray=[];
-    dArray=[];
-    ilArray=[];
-    var credentials = {
-      id: '13944c3c',
-      key: '5a09042c7587234cbd1adc10150874cf'
-    }
-    yummly.search({
-      credentials: credentials,
-      query: {
-        q: query
-      }
-    }, function (error, response, json) {
-      if (error) {
-        console.error(error);
-      } else if (response.statusCode === 200) {
-        console.log("FRUITCAKES");
-        console.log(json.matches[0].id);
-        var new_son = {
-          errCode: UserModel.ERR_BAD_CREDENTIALS,
-          count: result.rows[0].count
-        };
-        for (var i=0;i<10;i++) {
-            console.log("for loop");
-            recArray.push(json.matches[i].id);
-            recnameArray.push(json.matches[i].recipeName);
-            siuArray.push(json.matches[i].smallImageUrls);
-            dArray.push(json.matches[i].sourceDisplayName);
-            ilArray.push(json.matches[i].ingredients);
-        }
-        new_son = {
-        recipe_id : recArray, 
-        recipe_name: recnameArray, 
-        smallImageUrls:siuArray, 
-        details: dArray,
-        ingredient_list: ilArray
-        };
-        var format_son = JSON.stringify(new_son);
-        res.write(format_son);
-      }
-
-
-      /*
-      res.write(json.matches[0].recipeName);
-      res.write(json.matches[1].recipeName);
-      res.write(json.matches[2].recipeName);
-      res.write(json.matches[3].recipeName);
-      res.write(json.matches[4].recipeName);
-      res.write(json.matches[5].recipeName);
-      res.write(json.matches[6].recipeName);
-      res.write(json.matches[7].recipeName);
-      res.write(json.matches[8].recipeName);
-      res.write(json.matches[9].recipeName);
-      //res.end(json.matches[0].recipeName);
-      */
-      for (var i=0;i<10;i++) {
-            console.log("for loop");
-            recArray.push(json.matches[i].id);
-            recnameArray.push(json.matches[i].recipeName);
-            siuArray.push(json.matches[i].smallImageUrls);
-            dArray.push(json.matches[i].sourceDisplayName);
-            ilArray.push(json.matches[i].ingredients);
-    }
-      
-      new_son = {
-        recipe_id : recArray, 
-        recipe_name: recnameArray, 
-        smallImageUrls:siuArray, 
-        details: dArray,
-        ingredient_list: ilArray
-        };
-      var format_son = JSON.stringify(new_son);
-      res.end(format_son);
-    });
     //res.write('<html><body>');
     //res.write('CHOCO TACO');
     
--- searchcontroller.js
+++ searchcontroller.js
@@ -10,7 +10,81 @@ var search_controller = require('./searchcontroller.js');
 
 
 var search = function(param1){
-    return "PUPPY GUPPY";
+	jsonArray = []
+    recArray=[];
+    recnameArray=[];
+    siuArray=[];
+    dArray=[];
+    ilArray=[];
+    var credentials = {
+      id: '13944c3c',
+      key: '5a09042c7587234cbd1adc10150874cf'
+    }
+    yummly.search({
+      credentials: credentials,
+      query: {
+        q: query
+      }
+    }, function (error, response, json) {
+      if (error) {
+        console.error(error);
+      } else if (response.statusCode === 200) {
+        console.log("FRUITCAKES");
+        console.log(json.matches[0].id);
+        var new_son = {
+          errCode: UserModel.ERR_BAD_CREDENTIALS,
+          count: result.rows[0].count
+        };
+        for (var i=0;i<10;i++) {
+            recArray.push(json.matches[i].id);
+            recnameArray.push(json.matches[i].recipeName);
+            siuArray.push(json.matches[i].smallImageUrls);
+            dArray.push(json.matches[i].sourceDisplayName);
+            ilArray.push(json.matches[i].ingredients);
+        }
+        new_son = {
+        recipe_id : recArray, 
+        recipe_name: recnameArray, 
+        smallImageUrls:siuArray, 
+        details: dArray,
+        ingredient_list: ilArray
+        };
+        var format_son = JSON.stringify(new_son);
+        //res.write(format_son);
+      }
+
+
+      /*
+      res.write(json.matches[0].recipeName);
+      res.write(json.matches[1].recipeName);
+      res.write(json.matches[2].recipeName);
+      res.write(json.matches[3].recipeName);
+      res.write(json.matches[4].recipeName);
+      res.write(json.matches[5].recipeName);
+      res.write(json.matches[6].recipeName);
+      res.write(json.matches[7].recipeName);
+      res.write(json.matches[8].recipeName);
+      res.write(json.matches[9].recipeName);
+      //res.end(json.matches[0].recipeName);
+      */
+      for (var i=0;i<10;i++) {
+            recArray.push(json.matches[i].id);
+            recnameArray.push(json.matches[i].recipeName);
+            siuArray.push(json.matches[i].smallImageUrls);
+            dArray.push(json.matches[i].sourceDisplayName);
+            ilArray.push(json.matches[i].ingredients);
+    }
+      
+      new_son = {
+        recipe_id : recArray, 
+        recipe_name: recnameArray, 
+        smallImageUrls:siuArray, 
+        details: dArray,
+        ingredient_list: ilArray
+        };
+      var format_son = JSON.stringify(new_son);
+      return format_son;
+    });
 }
 
 var getRecipeData = function(param1, param2){
