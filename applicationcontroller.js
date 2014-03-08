3ffe7371cce6b1df204d06829adc13b619658705
Kevin Jia <kevin_jia191@yahoo.com>
2014-03-08 15:44:48 -0800

    edit

--- applicationcontroller.js
+++ applicationcontroller.js
@@ -21,7 +21,7 @@ app.get('/', function(req, res) {
   res.writeHead(200);
   res.write('<html><body>');
   res.write('<form action="login" method="post">Username <input type="text" name="username"><br>Password <input type="text" name="password"><input type="submit" value="Login" onclick=this.form.action="users/login"><input type="submit" value="add" onclick=this.form.action="users/signup">');
-  res.write('<input type="submit" value="getHistory" onclick=function(){this.form.action="/history">;this.form.method="get"}');
+  res.write('<input type="submit" value="getHistory" onclick=function(){this.form.action="/history"this.form.method="get"}>;');
   res.end('</form></body></html>');
 });
 
