975613540d5afcc6a82558518142e9eb371d9df6
jernchr11 <jernchr@berkeley.edu>
2014-03-08 20:32:33 -0800

    Made functions in controllers accessible for use

--- usercontroller.js
+++ usercontroller.js
@@ -3,12 +3,12 @@ var UserController = function(request) {
     this.request = request;
     // postRequest is a json containing the fields: user, password
     // TODO: encrypt the password
-    function signup(postRequest) {
+    this.signup = function (postRequest) {
         return {'errCode' : 1};
     }
 
     // postRequest is a json containing the fields: user, hashed_password
-    function login(postRequest) {
+    this.login = function (postRequest) {
         return {'errCode' : 1};
     }
 }
