a8b66603c4b78ea088c86ea687678ff510047f83
Kevin Jia <kevin_jia191@yahoo.com>
2014-03-12 15:59:25 -0700

    'history'

--- historycontroller.js
+++ historycontroller.js
@@ -3,33 +3,81 @@ var pg = require('pg');
 var HistoryController = function(request) {
 
     this.request = request;
+    this.SUCCESS = "SUCCESS";
+    this.dbERROR = "dbERROR";
     
     // postRequest is a json containing the fields: user, recipe_name, current_date, rating
-    this.make = function(postRequest, callback) {
-        var jsonObject = {errCode : 1};
-        var jsonForm = JSON.stringify(jsonObject);
-        callback(jsonForm);
+    this.make = function(postRequest, callback) { 
+        var jsonObject = {};       
+        
+        pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+            client.query("INSERT INTO HISTORY VALUES(" + postRequest.user + "," + postRequest.recipe_name + "," + postRequest.current_date + "," + postRequest.rating + ")", function(err, result){
+                done();
+                if(err){
+                    console.error(err);
+                    jsonObject.errCode = this.dbERROR;
+                    var jsonForm = JSON.stringify(jsonObject);
+                    callback(jsonForm);
+                    return;
+                }
+                
+                jsonObject.errCode = this.SUCCESS:
+                
+                var deletedEntry = "(" + postRequest.user + "," + postRequest.recipe_name + "," + postRequest.current_date + "," + postRequest.rating + ")";
+                
+                jsonObject.deletedEntry = deletedEntry;
+                var jsonForm = JSON.stringify(jsonObject);
+                callback(jsonForm);
+                return;
+            });
+        });
     }
 
     // postRequest is a json containing the fields: user
     this.getHistory = function(postRequest, callback) {
-        var jsonObject = {errCode : 1};
+        var jsonObject = {};
         var user = postRequest.user;
         pg.connect(process.env.DATABASE_URL, function(err, client, done) {
             client.query("SELECT * FROM history H WHERE username=\'" + user + "\'", function(err, result){
                 done();
-                if(err) return console.error(err);
+                if(err){
+                    console.error(err);
+                    jsonObject.errCode = this.dbERROR;
+                    var jsonForm = JSON.stringify(jsonObject);
+                    callback(jsonForm);
+                    return;
+                }
                 jsonObject.user = user;
                 jsonObject.userHistory = result.rows;
+                jsonObject.errCode = this.SUCCESS;
                 var jsonForm = JSON.stringify(jsonObject);
                 callback(jsonForm);
+                return;
             });
         });
     }
     
     // postRequest is a json containing the fields: user
     this.clearHistory = function(postRequest) {
-        return {errCode : 1};
+        var jsonObject = {};
+        
+        pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+            client.query("DELETE FROM HISTORY WHERE username=\' " + postRequest.user"\'", function(err, result){
+                done();
+                if(err){
+                    console.error(err);
+                    jsonObject.errCode = this.dbERROR;
+                    var jsonForm = JSON.stringify(jsonObject);
+                    callback(jsonForm);
+                    return;
+                }
+                
+                jsonObject.errCode = this.SUCCESS;
+                jsonObject.deletedEntry = "deletedAll";
+                var jsonForm = JSON.stringify(jsonObject);
+                callback(jsonForm);
+            });
+        });
     }
 }
 
