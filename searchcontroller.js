09fd74ca68b7ff39221dc76e3f7801a3ccba8127
akhilnambiar <akhilnambiar@berkeley.edu>
2014-03-12 22:30:25 -0700

    what is happening

--- searchcontroller.js
+++ searchcontroller.js
@@ -5,6 +5,7 @@ var searchcontroller = function(json){
     function search(param1, param2){
         return {errCode : 1};
     }
+    asdfsdfasdf
     
     function getRecipeData(param1, param2){
         return {errCode : 1};
