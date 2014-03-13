4e812e5e95adf11a7f26e7530bc7a257af7cca43
akhilnambiar <akhilnambiar@berkeley.edu>
2014-03-12 22:17:02 -0700

    with changes

--- HTTPHandler.js
+++ HTTPHandler.js
@@ -202,7 +202,7 @@ app.post('/recipes/search', function(req, res) {
     //example
     //process req, res to get stuff
     var q = req.body.q;
-    var searchController = new SearchController(null);
+    var sc = new SearchController(null);
     SearchController.search(q,function(result){
       res.end(result);
     });
