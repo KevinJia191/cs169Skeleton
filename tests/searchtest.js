1bdd13f4f2b21f1c19ee4a1bf0cad109ef5c2483
akhilnambiar <akhilnambiar@berkeley.edu>
2014-03-12 20:27:03 -0700

    hi

---
+++ tests/searchtest.js
@@ -0,0 +1,13 @@
+/*
+Searchcontroller Tests
+*/
+var searchController = require('../searchcontroller.js');
+exports['calculate'] = function (test) {
+    test.equal(4, 4);
+    test.done();
+};
+
+exports['calculate2'] = function (test) {
+    test.equal(4, 4);
+    test.done();
+};
