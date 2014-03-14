1a33c7a4c4fe3a6d99492b95dc764a20869ff51a
Kevin Jia <kevin_jia191@yahoo.com>
2014-03-14 12:49:06 -0700

    'hi'

--- HistoryController.js
+++ HistoryController.js
@@ -16,7 +16,6 @@ var HistoryController = function(res) {
     // postRequest is a json containing the fields: user, recipe_name, current_date, rating
     this.make = function(postRequest) {
         var jsonObject = {};     
-        console.log('hi1');
         var historyModel = new HistoryModel(postRequest.user, postRequest.recipe_name, postRequest.current_date, postRequest.rating);
         historyModel.connect();
         historyModel.make(function (resultingJson) {
