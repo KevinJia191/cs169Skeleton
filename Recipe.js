6fe6fa20f2513b634c388a9194f5c4f74424c607
Kevin Jia <kevin_jia191@yahoo.com>
2014-03-14 14:12:31 -0700

    'hi'

--- Recipe.js
+++ Recipe.js
@@ -25,8 +25,11 @@ function Recipe(username, recipe_name, date_created, rating){
         var makeQuery = "INSERT INTO HISTORY VALUES(" + this.username + "," + this.recipe_name + "," + this.current_date + "," + this.rating + ")"
         
         this.connection.query(testUserQuery, function(err, result){
+            console.log("1");
             if(result.rows.length>0){
+                console.log("2");
                 this.connection.query(testAlreadyMadeQuery, function(err, result){
+                    console.log("3");
                     if(result.rows.length == 0){
                         //Did not fail already made today check
                         this.connection.query(makeQuery, function(err, result){
