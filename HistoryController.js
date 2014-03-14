988c7f7afa0ce071d81329754492e74c9ee41910
Kevin Jia <kevin_jia191@yahoo.com>
2014-03-13 17:47:04 -0700

    'hi'

--- HistoryController.js
+++ HistoryController.js
@@ -6,6 +6,8 @@ var exists = fs.existsSync(file);
 var sqlite3 = require("sqlite3").verbose();
 var db = new sqlite3.Database(file);
 
+var HistoryModel = require('./Recipe.js');
+
 var HistoryController = function(request, unitTesting) {
 
     this.request = request;
@@ -16,68 +18,35 @@ var HistoryController = function(request, unitTesting) {
     
     // postRequest is a json containing the fields: user, recipe_name, current_date, rating
     this.make = function(postRequest, callback) {
-        var jsonObject = {};     
-  
         if(unitTesting){
             unitTestMake(postRequest, function(){
+                var jsonObject = {};     
                 jsonObject.errCode = SUCCESS;
                 jsonForm = JSON.stringify(jsonObject);
                 callback(jsonForm);
                 return;
             });
         } 
-        pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-            client.query("SELECT * FROM users U WHERE username=\'" + postRequest.user + "/'", function(err, result){
-                if(result.rows.length>0){
-                    client.query("SELECT * FROM history H WHERE username=\'" + postRequest.user + "\'" + "AND recipe_name=\'" + postRequest.recipe_name + "\'" + "AND current_date=" + postRequest.current_date, function(err, result){
-                        if(result.rows.length == 0){
-                            //Did not fail already made today check
-                            client.query("INSERT INTO HISTORY VALUES(" + postRequest.user + "," + postRequest.recipe_name + "," + postRequest.current_date + "," + postRequest.rating + ")", function(err, result){
-                                done();
-                                if(err){
-                                    console.error(err);
-                                    jsonObject.errCode = ERROR;
-                                    var jsonForm = JSON.stringify(jsonObject);
-                                    callback(jsonForm);
-                                    return;
-                                }
-                                
-                                jsonObject.errCode = SUCCESS;
-                                
-                                var madeEntry = "(" + postRequest.user + "," + postRequest.recipe_name + "," + postRequest.current_date + "," + postRequest.rating + ")";
-                                
-                                jsonObject.madeEntry = madeEntry;
-                                var jsonForm = JSON.stringify(jsonObject);
-                                callback(jsonForm);
-                                return;
-                            });
-                        }
-                        else{
-                            jsonObject.errCode = ERR_CREATED_ALREADY;
-                            var jsonForm = JSON.stringify(jsonObject);
-                            callback(jsonForm);
-                            return;
-                        }
-                    });
-                }
-                else{
-                    jsonObject.errCode = INVAL_USER;
-                    var jsonForm = JSON.stringify(jsonObject);
-                    callback(jsonForm);
-                    return;
-                }
-            });
-        });
+        
+        var historyModel = new HistoryModel(postRequest.user, postRequest.recipe_name, postRequest.current_date, postRequest.rating);
+        historyModel.connect();
+        historyModel.make(function(jsonForm){
+            callback(jsonForm);
+        }
     }
 
     // postRequest is a json containing the fields: user
     this.getHistory = function(postRequest, callback) {
+        var jsonObject = {};
         if(unitTesting){
-            unitTestGetHistory(postRequest, function(){
+            unitTestGetHistory(postRequest, function(history){
+                jsonObject.errCode = SUCCESS;
+                jsonObject.history = history;
+                jsonForm = JSON.stringify(jsonObject);
+                callback(jsonForm);
                 return;
             });
         } 
-        var jsonObject = {};
         var user = postRequest.user;
         pg.connect(process.env.DATABASE_URL, function(err, client, done) {
             client.query("SELECT * FROM users U WHERE username=\'" + postRequest.user + "/'", function(err, result){
@@ -112,6 +81,9 @@ var HistoryController = function(request, unitTesting) {
     this.clearHistory = function(postRequest, callback) {
         if(unitTesting){
             unitTestClearHistory(postRequest, function(){
+                jsonObject.errCode = SUCCESS;
+                jsonForm = JSON.stringify(jsonObject);
+                callback(jsonForm);
                 return;
             });
         } 
@@ -141,20 +113,20 @@ var HistoryController = function(request, unitTesting) {
                 db.run("Create table users (username text primary key, hashed_password text);");
                 db.run("Create table ingredients (username text,ingredient_name text,expiration_date text,quantity decimal check(quantity>0),unit text,primary key(username,ingredient_name,expiration_date));");
                 db.run("Create table history (username text references users(username),recipe_name text,dateCreated text,rating int check(rating > 0 AND rating <= 5),primary key(username,recipe_name,dateCreated));");
-            
-                var stmt = db.prepare("INSERT INTO users VALUES (?)");
+                            
+                db.run("INSERT INTO users values(\"user1\",\"pass1\"");
+                db.run("INSERT INTO users values(\"user2\",\"pass2\"");
+                db.run("INSERT INTO users values(\"user3\",\"pass3\"");
+
+                db.run("INSERT INTO history values \"user1\",\"Apple Soup\",\"2/2/2\",3");
+                db.run("INSERT INTO history values \"user1\",\"Banana Soup\",\"3/3/3\",2");
+                db.run("INSERT INTO history values \"user2\",\"Chicken Soup\",\"4/4/4\",4");
+                db.run("INSERT INTO history values \"user3\",\"Dough Soup\",\"5/5/5\",5");
                 
-                for (var i = 0; i < 10; i++) {
-                    stmt.run("user" + i, "pass" + i);
-                }
-                stmt.finalize();
-                
-                stmt = db.prepare("INSERT INTO history values (?)");
-                stmt.run("user1","Apple Soup","2/2/2",3);
-                stmt.run("user1","Banana Soup","3/3/3",2);
-                stmt.run("user2","Chicken Soup","4/4/4",4);
-                stmt.run("user3","Dough Soup","5/5/5",5);
-                stmt.finalize();
+                db.each("SELECT username, recipe_name, dateCreated, rating FROM history WHERE user =\'"+postRequest.user+"\'", function(err, row) {
+                console.log("row is " + row);
+                console.log("" + row.username + ", " + row.recipe_name + ", " + row.dateCreated + ", " + row.rating);
+                });
             }
         });
         db.close();
@@ -178,16 +150,34 @@ var HistoryController = function(request, unitTesting) {
     }
     
     var unitTestGetHistory = function(postRequest, callback){
+        var history = [];
+        var users = [];
+        var recipe_names = [];
+        var date_createds = [];
+        var ratings = [];
         db.serialize(function() {
-            db.each("SELECT username AS user, recipe_name, dateCreated, rating FROM history WHERE user =\'"+postRequest.user+"\'", function(err, row) {
-                console.log(row.user + ", " + row.recipe_name + ", " + row.dateCreated + ", " + row.rating);
+            db.each("SELECT username, recipe_name, dateCreated, rating FROM history WHERE user =\'"+postRequest.user+"\'", function(err, row) {
+                console.log("row is " + row);
+                console.log("" + row.username + ", " + row.recipe_name + ", " + row.dateCreated + ", " + row.rating);
+                users.push(row.user);
+                recipe_names.push(row.recipe_name);
+                date_createds.push(row.dateCreated);
+                ratings.push(row.rating);
             });
         });
         db.close();
-        callback();
+        history.push(users);
+        history.push(recipe_names);
+        history.push(date_createds);
+        history.push(ratings);
+        callback(history);
     }
     
     var unitTestClearHistory = function(postRequest, callback){
+        db.serialize(function() {
+            db.run("DELETE FROM HISTORY WHERE username=\' " + postRequest.user + "\'");
+        });
+        db.close();
         callback();
     }
 }
--- tests/historyUnitTest.js
+++ tests/historyUnitTest.js
@@ -1,7 +1,12 @@
 var HistoryController = require('../HistoryController.js');
 
+var fs = require("fs");
+var file = "test.db";
+var exists = fs.existsSync(file);
+var sqlite3 = require("sqlite3").verbose();
+var db = new sqlite3.Database(file);
 
-exports['makeTest'] = function (test) {
+exports['makeErrorCodetest'] = function (test) {
     var stubJson = {user : "testUser",
                     recipe_name : "Onion Soup",
                     current_date : "2/2/2",
@@ -29,5 +34,39 @@ exports['makeTest'] = function (test) {
         test.done();
     });
     
+};
+
+exports['maketest'] = function (test) {
+    var stubJson = {user : "user1",
+                    recipe_name : "Onion Soup",
+                    current_date : "2/2/2",
+                    rating : 3
+                   };
+
+    
+    var unitTesting = true;
+	var historyController = new HistoryController(null, unitTesting);
+    
+    //Sets up DB if never called before, else does nothing
+    historyController.unitTestSetup();
+
+    historyController.make(stubJson, function(resultingJson){
+        //console.log(resultingJson);
+        var errCodeNameLength = "{\"errCode\":".length;
+        var errCodeStringPos = (resultingJson.indexOf("errCode"));
+        var temp = (resultingJson.substr(1+errCodeNameLength, resultingJson.length)).indexOf("\"");
+        //console.log((resultingJson.substr(1+errCodeNameLength, resultingJson.length)));
+        var errCode = resultingJson.substr(1+errCodeNameLength,temp);
+
+        //console.log("errCode is " + errCode);
+        //console.log(errCode == "SUCCESS");
+        test.equal(errCode, "SUCCESS", "ERRCODE WAS NOT SUCCESS");
+    });
+    
+    var stubJson2 = {user : "user1"};
+    historyController.getHistory(stubJson2, function(resultingJson){
+        console.log(resultingJson);
+        test.done();
+    });
 };
 
