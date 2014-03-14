8181d01221b5938884ba8277907150f501740227
Kevin Jia <kevin_jia191@yahoo.com>
2014-03-14 14:36:46 -0700

    'hi'

--- Recipe.js
+++ Recipe.js
@@ -22,7 +22,7 @@ function Recipe(username, recipe_name, date_created, rating){
         
         var testUserQuery = "SELECT * FROM users U WHERE U.username=\'" + this.username + "\'";
         var testAlreadyMadeQuery = "SELECT * FROM history H WHERE H.username=\'" + this.username + "\'" + "AND H.recipe_name=\'" + this.recipe_name + "\'" + "AND H.current_date='" + this.current_date +"'";
-        var makeQuery = "INSERT INTO HISTORY VALUES('" + this.username + "','" + this.recipe_name + "','" + this.current_date + "','" + this.rating + "')";
+        var makeQuery = "INSERT INTO HISTORY VALUES('" + this.username + "','" + this.recipe_name + "','" + this.current_date + "'," + this.rating + ")";
         
         console.log(testUserQuery);
         console.log(testAlreadyMadeQuery);
