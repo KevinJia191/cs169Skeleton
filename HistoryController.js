2d94667430d9cf85d3b7d0f621d30e6c3ecae738
Kevin Jia <kevin_jia191@yahoo.com>
2014-03-13 17:58:01 -0700

    'hi'

--- HistoryController.js
+++ HistoryController.js
@@ -18,20 +18,57 @@ var HistoryController = function(request, unitTesting) {
     
     // postRequest is a json containing the fields: user, recipe_name, current_date, rating
     this.make = function(postRequest, callback) {
+        var jsonObject = {};     
+  
         if(unitTesting){
             unitTestMake(postRequest, function(){
-                var jsonObject = {};     
                 jsonObject.errCode = SUCCESS;
                 jsonForm = JSON.stringify(jsonObject);
                 callback(jsonForm);
                 return;
             });
-        }
-        
-        var historyModel = new HistoryModel(postRequest.user, postRequest.recipe_name, postRequest.current_date, postRequest.rating);
-        historyModel.connect();
-        historyModel.make(function(jsonForm){
-            callback(jsonForm);
+        } 
+        pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+            client.query("SELECT * FROM users U WHERE username=\'" + postRequest.user + "/'", function(err, result){
+                if(result.rows.length>0){
+                    client.query("SELECT * FROM history H WHERE username=\'" + postRequest.user + "\'" + "AND recipe_name=\'" + postRequest.recipe_name + "\'" + "AND current_date=" + postRequest.current_date, function(err, result){
+                        if(result.rows.length == 0){
+                            //Did not fail already made today check
+                            client.query("INSERT INTO HISTORY VALUES(" + postRequest.user + "," + postRequest.recipe_name + "," + postRequest.current_date + "," + postRequest.rating + ")", function(err, result){
+                                done();
+                                if(err){
+                                    console.error(err);
+                                    jsonObject.errCode = ERROR;
+                                    var jsonForm = JSON.stringify(jsonObject);
+                                    callback(jsonForm);
+                                    return;
+                                }
+                                
+                                jsonObject.errCode = SUCCESS;
+                                
+                                var madeEntry = "(" + postRequest.user + "," + postRequest.recipe_name + "," + postRequest.current_date + "," + postRequest.rating + ")";
+                                
+                                jsonObject.madeEntry = madeEntry;
+                                var jsonForm = JSON.stringify(jsonObject);
+                                callback(jsonForm);
+                                return;
+                            });
+                        }
+                        else{
+                            jsonObject.errCode = ERR_CREATED_ALREADY;
+                            var jsonForm = JSON.stringify(jsonObject);
+                            callback(jsonForm);
+                            return;
+                        }
+                    });
+                }
+                else{
+                    jsonObject.errCode = INVAL_USER;
+                    var jsonForm = JSON.stringify(jsonObject);
+                    callback(jsonForm);
+                    return;
+                }
+            });
         });
     }
 
--- Recipe.js
+++ Recipe.js
@@ -1,6 +1,3 @@
-var pg = require('pg');
-
-
 //should extend the ActiveRecord class
 function Recipe(username, recipe_name, date_created, rating){
     this.username = username;
@@ -8,50 +5,7 @@ function Recipe(username, recipe_name, date_created, rating){
     this.date_created = date_created;
     this.rating = rating;
     
-    this.make = function(callback) {
-        var jsonObject = {};     
-        pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-            client.query("SELECT * FROM users U WHERE username=\'" + postRequest.user + "/'", function(err, result){
-                if(result.rows.length>0){
-                    client.query("SELECT * FROM history H WHERE username=\'" + postRequest.user + "\'" + "AND recipe_name=\'" + postRequest.recipe_name + "\'" + "AND current_date=" + postRequest.current_date, function(err, result){
-                        if(result.rows.length == 0){
-                            //Did not fail already made today check
-                            client.query("INSERT INTO HISTORY VALUES(" + postRequest.user + "," + postRequest.recipe_name + "," + postRequest.current_date + "," + postRequest.rating + ")", function(err, result){
-                                done();
-                                if(err){
-                                    console.error(err);
-                                    jsonObject.errCode = ERROR;
-                                    var jsonForm = JSON.stringify(jsonObject);
-                                    callback(jsonForm);
-                                    return;
-                                }
-                                
-                                jsonObject.errCode = SUCCESS;
-                                
-                                var madeEntry = "(" + postRequest.user + "," + postRequest.recipe_name + "," + postRequest.current_date + "," + postRequest.rating + ")";
-                                
-                                jsonObject.madeEntry = madeEntry;
-                                var jsonForm = JSON.stringify(jsonObject);
-                                callback(jsonForm);
-                                return;
-                            });
-                        }
-                        else{
-                            jsonObject.errCode = ERR_CREATED_ALREADY;
-                            var jsonForm = JSON.stringify(jsonObject);
-                            callback(jsonForm);
-                            return;
-                        }
-                    });
-                }
-                else{
-                    jsonObject.errCode = INVAL_USER;
-                    var jsonForm = JSON.stringify(jsonObject);
-                    callback(jsonForm);
-                    return;
-                }
-            });
-        });
+    this.add = function(callback) {
     }
 
     this.remove = function(callback) {
