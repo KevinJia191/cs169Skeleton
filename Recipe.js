2278e2e224d447fe5808e9b127b85ab6b0a53114
Kevin Jia <kevin_jia191@yahoo.com>
2014-03-14 12:46:45 -0700

    'hi'

--- Recipe.js
+++ Recipe.js
@@ -24,12 +24,12 @@ function Recipe(username, recipe_name, date_created, rating){
         var testAlreadyMadeQuery = "SELECT * FROM history H WHERE H.username=\'" + this.username + "\'" + "AND H.recipe_name=\'" + this.recipe_name + "\'" + "AND H.current_date=" + this.current_date
         var makeQuery = "INSERT INTO HISTORY VALUES(" + this.username + "," + this.recipe_name + "," + this.current_date + "," + this.rating + ")"
         
-        self.connection.query(testUserQuery, function(err, result){
+        this.connection.query(testUserQuery, function(err, result){
             if(result.rows.length>0){
-                self.connection.query(testAlreadyMadeQuery, function(err, result){
+                this.connection.query(testAlreadyMadeQuery, function(err, result){
                     if(result.rows.length == 0){
                         //Did not fail already made today check
-                        self.connection.query(makeQuery, function(err, result){
+                        this.connection.query(makeQuery, function(err, result){
                             if(err){
                                 console.error(err);
                                 jsonObject.errCode = CONSTANTS.ERROR;
@@ -65,7 +65,7 @@ function Recipe(username, recipe_name, date_created, rating){
     this.clearAllHistoryFromUser = function(callback) {
         var jsonObject = {};
         var deleteQuery = "DELETE FROM HISTORY WHERE username=\' " + this.username + "\'"
-        self.connection.query(deleteQuery, function(err, result){
+        this.connection.query(deleteQuery, function(err, result){
             if(err){
                 console.error(err);
                 jsonObject.errCode = CONSTANTS.ERROR;
@@ -84,9 +84,9 @@ function Recipe(username, recipe_name, date_created, rating){
         var jsonObject = {};
         var testUserQuery = "SELECT * FROM users U WHERE U.username=\'" + this.username + "/'";
         var getHistoryQuery = "SELECT * FROM history H WHERE H.username=\'" + this.username + "\'";
-        self.connection.query(testUserQuery, function(err, result){
+        this.connection.query(testUserQuery, function(err, result){
             if(result.rows.length > 0){
-                self.connection.query(getHistoryQuery, function(err, result){
+                this.connection.query(getHistoryQuery, function(err, result){
                     if(err){
                         console.error(err);
                         jsonObject.errCode = CONSTANTS.ERROR;
