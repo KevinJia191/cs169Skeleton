769eddaca2e8ec3a3d33af87070d3561dcb65350
Kevin Jia <kevin_jia191@yahoo.com>
2014-03-13 18:43:08 -0700

    'hi'

--- HistoryController.js
+++ HistoryController.js
@@ -28,47 +28,11 @@ var HistoryController = function(request, unitTesting) {
                 return;
             });
         } 
-        pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-            client.query("SELECT * FROM users U WHERE username=\'" + postRequest.user + "/'", function(err, result){
-                if(result.rows.length>0){
-                    client.query("SELECT * FROM history H WHERE username=\'" + postRequest.user + "\'" + "AND recipe_name=\'" + postRequest.recipe_name + "\'" + "AND current_date=" + postRequest.current_date, function(err, result){
-                        if(result.rows.length == 0){
-                            //Did not fail already made today check
-                            client.query("INSERT INTO HISTORY VALUES(" + postRequest.user + "," + postRequest.recipe_name + "," + postRequest.current_date + "," + postRequest.rating + ")", function(err, result){
-                                done();
-                                if(err){
-                                    console.error(err);
-                                    jsonObject.errCode = ERROR;
-                                    var jsonForm = JSON.stringify(jsonObject);
-                                    callback(jsonForm);
-                                    return;
-                                }
-                                
-                                jsonObject.errCode = SUCCESS;
-                                
-                                var madeEntry = "(" + postRequest.user + "," + postRequest.recipe_name + "," + postRequest.current_date + "," + postRequest.rating + ")";
-                                
-                                jsonObject.madeEntry = madeEntry;
-                                var jsonForm = JSON.stringify(jsonObject);
-                                callback(jsonForm);
-                                return;
-                            });
-                        }
-                        else{
-                            jsonObject.errCode = ERR_CREATED_ALREADY;
-                            var jsonForm = JSON.stringify(jsonObject);
-                            callback(jsonForm);
-                            return;
-                        }
-                    });
-                }
-                else{
-                    jsonObject.errCode = INVAL_USER;
-                    var jsonForm = JSON.stringify(jsonObject);
-                    callback(jsonForm);
-                    return;
-                }
-            });
+        var historyModel = new HistoryModel(postRequest.user, postRequest.recipe_name, postRequest.current_date, postRequest.rating);
+        historyModel.connect();
+        historyModel.make(postRequest, function (resultingJson) {
+            historyModel.end();
+            callback(resultingJson);
         });
     }
 
@@ -84,38 +48,17 @@ var HistoryController = function(request, unitTesting) {
                 return;
             });
         } 
-        var user = postRequest.user;
-        pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-            client.query("SELECT * FROM users U WHERE username=\'" + postRequest.user + "/'", function(err, result){
-                if(result.rows.length > 0){
-                    client.query("SELECT * FROM history H WHERE username=\'" + user + "\'", function(err, result){
-                        done();
-                        if(err){
-                            console.error(err);
-                            jsonObject.errCode = ERROR;
-                            var jsonForm = JSON.stringify(jsonObject);
-                            callback(jsonForm);
-                            return;
-                        }
-                        jsonObject.userHistory = result.rows;
-                        jsonObject.errCode = SUCCESS;
-                        var jsonForm = JSON.stringify(jsonObject);
-                        callback(jsonForm);
-                        return;
-                    });
-                }
-                else{
-                    jsonObject.errCode = INVAL_USER;
-                    var jsonForm = JSON.stringify(jsonObject);
-                    callback(jsonForm);
-                    return;
-                }
-            });
+        var historyModel = new HistoryModel(postRequest.user, postRequest.recipe_name, postRequest.current_date, postRequest.rating);
+        historyModel.connect();
+        historyModel.getAllFromUser(postRequest, function (resultingJson) {
+            historyModel.end();
+            callback(resultingJson);
         });
     }
     
     // postRequest is a json containing the fields: user
     this.clearHistory = function(postRequest, callback) {
+        var jsonObject = {};
         if(unitTesting){
             unitTestClearHistory(postRequest, function(){
                 jsonObject.errCode = SUCCESS;
@@ -124,23 +67,11 @@ var HistoryController = function(request, unitTesting) {
                 return;
             });
         } 
-        var jsonObject = {};
-        pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-            client.query("DELETE FROM HISTORY WHERE username=\' " + postRequest.user + "\'", function(err, result){
-                done();
-                if(err){
-                    console.error(err);
-                    jsonObject.errCode = ERROR;
-                    var jsonForm = JSON.stringify(jsonObject);
-                    callback(jsonForm);
-                    return;
-                }
-                
-                jsonObject.errCode = SUCCESS;
-                jsonObject.deletedEntry = "deletedAll";
-                var jsonForm = JSON.stringify(jsonObject);
-                callback(jsonForm);
-            });
+        var historyModel = new HistoryModel(postRequest.user, postRequest.recipe_name, postRequest.current_date, postRequest.rating);
+        historyModel.connect();
+        historyModel.clearAllFromUser(postRequest, function (resultingJson) {
+            historyModel.end();
+            callback(resultingJson);
         });
     }
     /*
--- Recipe.js
+++ Recipe.js
@@ -1,25 +1,122 @@
+var pg = require('pg');
+
 //should extend the ActiveRecord class
 function Recipe(username, recipe_name, date_created, rating){
     this.username = username;
     this.recipe_name = recipe_name;
     this.date_created = date_created;
     this.rating = rating;
+    this.connection=null;
     
-    this.add = function(callback) {
+    this.add = function(postRequest, callback) {
+        var jsonObject = {};     
+            this.connection.query("SELECT * FROM users U WHERE username=\'" + postRequest.user + "/'", function(err, result){
+                if(result.rows.length>0){
+                    this.connection.query("SELECT * FROM history H WHERE username=\'" + postRequest.user + "\'" + "AND recipe_name=\'" + postRequest.recipe_name + "\'" + "AND current_date=" + postRequest.current_date, function(err, result){
+                        if(result.rows.length == 0){
+                            //Did not fail already made today check
+                            this.connection.query("INSERT INTO HISTORY VALUES(" + postRequest.user + "," + postRequest.recipe_name + "," + postRequest.current_date + "," + postRequest.rating + ")", function(err, result){
+                                done();
+                                if(err){
+                                    console.error(err);
+                                    jsonObject.errCode = ERROR;
+                                    var jsonForm = JSON.stringify(jsonObject);
+                                    callback(jsonForm);
+                                    return;
+                                }
+                                
+                                jsonObject.errCode = SUCCESS;
+                                
+                                var madeEntry = "(" + postRequest.user + "," + postRequest.recipe_name + "," + postRequest.current_date + "," + postRequest.rating + ")";
+                                
+                                jsonObject.madeEntry = madeEntry;
+                                var jsonForm = JSON.stringify(jsonObject);
+                                callback(jsonForm);
+                                return;
+                            });
+                        }
+                        else{
+                            jsonObject.errCode = ERR_CREATED_ALREADY;
+                            var jsonForm = JSON.stringify(jsonObject);
+                            callback(jsonForm);
+                            return;
+                        }
+                    });
+                }
+                else{
+                    jsonObject.errCode = INVAL_USER;
+                    var jsonForm = JSON.stringify(jsonObject);
+                    callback(jsonForm);
+                    return;
+                }
+            });
     }
 
-    this.remove = function(callback) {
+    this.clearAllHistoryFromUser = function(postRequest, callback) {
+        var jsonObject = {};
+            this.connection.query("DELETE FROM HISTORY WHERE username=\' " + postRequest.user + "\'", function(err, result){
+                done();
+                if(err){
+                    console.error(err);
+                    jsonObject.errCode = ERROR;
+                    var jsonForm = JSON.stringify(jsonObject);
+                    callback(jsonForm);
+                    return;
+                }
+                
+                jsonObject.errCode = SUCCESS;
+                jsonObject.deletedEntry = "deletedAll";
+                var jsonForm = JSON.stringify(jsonObject);
+                callback(jsonForm);
+            });
     }
 
-    this.get = function(callback){
+    this.getAllHistoryFromUser = function(postRequest, callback){
+            this.connection.query("SELECT * FROM users U WHERE username=\'" + postRequest.user + "/'", function(err, result){
+                if(result.rows.length > 0){
+                    this.connection.query("SELECT * FROM history H WHERE username=\'" + postRequest.user + "\'", function(err, result){
+                        done();
+                        if(err){
+                            console.error(err);
+                            jsonObject.errCode = ERROR;
+                            var jsonForm = JSON.stringify(jsonObject);
+                            callback(jsonForm);
+                            return;
+                        }
+                        jsonObject.userHistory = result.rows;
+                        jsonObject.errCode = SUCCESS;
+                        var jsonForm = JSON.stringify(jsonObject);
+                        callback(jsonForm);
+                        return;
+                    });
+                }
+                else{
+                    jsonObject.errCode = INVAL_USER;
+                    var jsonForm = JSON.stringify(jsonObject);
+                    callback(jsonForm);
+                    return;
+                }
+            });
     }
     
     this.contains = function(callback) {
     }
 
     this.setDatabaseModel = function(model) {
+        this.connection = model;
     }
 
     this.setSort = function(sortby) {
     }
+    
+    this.connect = function() {
+        this.connection = new pg.Client(process.env.DATABASE_URL);
+        this.connection.connect();
+    }
+    
+    this.end = function(){
+        this.connection.end();
+    }
+    
+    
 }
