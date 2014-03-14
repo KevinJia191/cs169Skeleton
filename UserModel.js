f01ba4c69bbb5b4c198ba5a27385dc1926626e54
Kevin Jia <kevin_jia191@yahoo.com>
2014-03-13 23:48:55 -0700

    'wee'

--- UserModel.js
+++ UserModel.js
@@ -22,16 +22,12 @@ function UserModel(username, password){
             }
             //add user to database
             else{
-
                 this.connection.query(inputQuery, function () {
-
                     jsonObject.errCode = UserModel.SUCCESS;
                     var jsonForm = JSON.stringify(jsonObject);
                     callback(jsonForm);
                     return;
                 });
-
-                
             }// ELSE END
         });//TestUserQuery END
     }// signup END
