7ff9d669313e9622a553877867aaabc8a464b959
akhilnambiar <akhilnambiar@berkeley.edu>
2014-03-30 18:58:49 -0700

    recipie

--- SearchController.js
+++ SearchController.js
@@ -75,6 +75,32 @@ var searchcontroller = function(json){
 
     */
     function getRecipeData(recipie_id, callback){
+        var credentials = {
+          id: '13944c3c',
+          key: '5a09042c7587234cbd1adc10150874cf'
+        }
+        yummly.search({ // calling search first to get a recipe id
+          credentials: credentials,
+          query: {
+            q: 'pasta'
+          }
+        }, function (error, response, json) {
+          if (error) {
+            console.error(error);
+          } else if (response.statusCode === 200) {
+            yummly.recipe({
+              credentials: credentials,
+              id: json.matches[0].id // id of the first recipe returned by search
+            }, function (error, response, json) {
+              if (error) {
+                console.error(error);
+              } else {
+                console.log(json);
+              }
+            });
+          }
+        });
+        /*
         yummly.recipe({
           credentials: credentials,
           id: recipie_id // id of the first recipe returned by search
@@ -88,5 +114,6 @@ var searchcontroller = function(json){
         });
         return {errCode : 1};
     }
+    */
 }
 module.exports = searchcontroller;
