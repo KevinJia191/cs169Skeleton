9eadd24068f601039f8415b9128d77fafdb6fbc0
jernchr11 <jernchr@berkeley.edu>
2014-03-30 17:30:45 -0700

    ...

--- UserController.js
+++ UserController.js
@@ -7,11 +7,6 @@ var PostgreSQLDatabaseModel = require('./PostgreSQLDatabaseModel.js');
 var PostgreSQLParser = require('./PostgreSQLParser.js');
 
 var UserController = function(res) {
-    // postRequest is a json containing the fields: user, password
-    // TODO: encrypt the password
-    //{errCode:SUCCESS} if success
-    //{errCode:ERR_USER_EXISTS} if user exists
-    //{errCode:ERROR} if name is null or longer than 128 characters
     this.signup = function (postRequest) {
 	var db = new PostgreSQLDatabaseModel(process.env.DATABASE_URL)
         var userModel = new UserModel(postRequest.user, postRequest.password);
@@ -26,12 +21,8 @@ var UserController = function(res) {
 	});
     }
 
-    // postRequest is a json containing the fields: user, hashed_password
-    //{errCode:SUCCESS} if login succeeds
-    //{errcode:ERR_INVAL_CRED}
     this.login = function (postRequest) {
         console.log(postRequest);
-
 	var db = new PostgreSQLDatabaseModel(process.env.DATABASE_URL);
         var userModel = new UserModel(postRequest.user, postRequest.password);
         userModel.setDatabaseModel(db);
