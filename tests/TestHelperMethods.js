a43a8cea6bdbb30c946cda29d109916ff81f4a71
jernchr11 <jernchr@berkeley.edu>
2014-04-02 15:42:14 -0700

    history unit tests

--- tests/TestHelperMethods.js
+++ tests/TestHelperMethods.js
@@ -5,7 +5,7 @@ helperMethods.doSetup = function(db, callback) {
     var createUsers = "Create table users (username text primary key, hashed_password text, salt text);"
     db.query(createUsers, function(err, results) {
 	db.query("Create table ingredients (username text references users(username),ingredient_name text,expiration_date text,quantity decimal check(quantity>0),unit text, primary key(username,ingredient_name,expiration_date)); ", function(err, results) {
-	    var createHistory = "Create table history (username text references users(username), recipe_name text, dateCreated text, rating int check(rating > 0 AND rating <= 5), primary key(username,recipe_name,dateCreated));";
+	    var createHistory = "Create table history (username text references users(username), recipe_name text, dateCreated text, primary key(username,recipe_name,dateCreated));";
 	    db.query(createHistory, function(err, results) {
 		callback();
 	    });
--- tests/historyUnitTest.js
+++ tests/historyUnitTest.js
@@ -9,7 +9,7 @@ var doSetup = HelperMethods.setupAndCreateUsers;
 
 exports["testMakeRecipe"] = function(test){
     var db = new SQLite3Model();
-    test.expect(2);
+    test.expect(5);
     doSetup(db, function() {
         var historyModel = new HistoryModel('jernchr', 'OnionSoup', '5/21/17');
         historyModel.setDatabaseModel(db);
@@ -20,8 +20,94 @@ exports["testMakeRecipe"] = function(test){
             db.query("select * from "+ Constants.HISTORY_TABLE, function(err, rows) {
                 db.end();
                 test.equal(rows.length, 1, "Should be just one recipe made");
+		var exp = { username: 'jernchr', recipe_name: 'OnionSoup', dateCreated: '5/21/17'};
+		var row = rows[0];
+		testRecipeEqual(row, exp, test);
                 test.done();
             });
         });
     });
 };
+
+exports["testMakeTwoRecipes"] = function(test){
+    var db = new SQLite3Model();
+    test.expect(9);
+    doSetup(db, function() {
+        var historyModel = new HistoryModel('jernchr', 'OnionSoup', '5/21/17');
+        historyModel.setDatabaseModel(db);
+        historyModel.setParser(new SQLite3Parser());
+        db.connect();
+        historyModel.make(function(err, result) {
+	    test.equal(err, Constants.SUCCESS, "Make should be success");
+	    var historyModel = new HistoryModel('jernchr', 'Chicken Parmesan', '5/21/19');
+            historyModel.setDatabaseModel(db);
+            historyModel.setParser(new SQLite3Parser());
+	    historyModel.make(function(err, result) {
+		test.equal(err, Constants.SUCCESS, "Make should be success");
+		db.query("select * from "+ Constants.HISTORY_TABLE, function(err, rows) {
+                    db.end();
+                    test.equal(rows.length, 2, "Should be two recipes made");
+		    var exp = { username: 'jernchr', recipe_name: 'OnionSoup', dateCreated: '5/21/17'};
+		    var exp2 = { username: 'jernchr', recipe_name: 'Chicken Parmesan', dateCreated: '5/21/19'};
+		    if (rows[0]["recipe_name"] == "Chicken Parmesan") {
+
+			testRecipeEqual(rows[0], exp2, test);
+			testRecipeEqual(rows[1], exp, test);
+		    }
+		    else {
+			testRecipeEqual(rows[0], exp, test);
+			testRecipeEqual(rows[1], exp2, test);
+		    }
+                    test.done();
+		});
+	    });
+        });
+    });
+};
+
+exports["getHistory"] = function(test){
+    var db = new SQLite3Model();
+    test.expect(9);
+    doSetup(db, function() {
+        var historyModel = new HistoryModel('jernchr', 'OnionSoup', '5/21/17');
+        historyModel.setDatabaseModel(db);
+        historyModel.setParser(new SQLite3Parser());
+        db.connect();
+        historyModel.make(function(err, result) {
+	    var historyModel = new HistoryModel('jernchr', 'Chicken Parmesan', '5/21/19');
+            historyModel.setDatabaseModel(db);
+            historyModel.setParser(new SQLite3Parser());
+	    historyModel.make(function(err, result) {
+		test.equal(err, Constants.SUCCESS, "Make should be success");
+		var historyModel = new HistoryModel('jernchr');
+		historyModel.setDatabaseModel(db);
+		historyModel.setParser(new SQLite3Parser());
+		historyModel.getAllHistoryFromUser(function(err, results) {
+		    test.equal(err, Constants.SUCCESS, "Get history should be success");
+		    test.equal(results.length, 2);
+		    var exp = { username: 'jernchr', recipe_name: 'OnionSoup', dateCreated: '5/21/17'};
+		    var exp2 = { username: 'jernchr', recipe_name: 'Chicken Parmesan', dateCreated: '5/21/19'};
+		    if (results[0].fields["recipe_name"] == "Chicken Parmesan") {
+			console.log(results[1].fields);
+			testRecipeEqual(results[0].fields, exp2, test);
+			testRecipeEqual(results[1].fields, exp, test);
+		    }
+		    else {
+			testRecipeEqual(results[1].fields, exp2, test);
+			testRecipeEqual(results[0].fields, exp, test);
+		    }
+		    test.done();
+		});
+	    });
+        });
+    });
+};
+
+
+
+
+function testRecipeEqual(row, exp, test) {
+    test.equal(row.username, exp.username, "Failed");
+    test.equal(row.recipe_name, exp.recipe_name);
+    test.equal(row.dateCreated, exp.dateCreated);
+}
