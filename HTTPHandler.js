41eccd5b258bcb131b0b4525206b4a42b5a99c11
akhilnambiar <akhilnambiar@berkeley.edu>
2014-04-02 09:43:46 -0700

    hello

--- HTTPHandler.js
+++ HTTPHandler.js
@@ -216,8 +216,8 @@ app.post('/recipes/search', function(req, res) {
     //example
     //process req, res to get stuff
     var q = req.body.q;
-    //var searchController = new SearchController(null);
-    var searchController = new MockFoodAPI();
+    var searchController = new SearchController(null);
+    //var searchController = new MockFoodAPI();
     searchController.search(q,function(result){
       res.end(result);
     });
