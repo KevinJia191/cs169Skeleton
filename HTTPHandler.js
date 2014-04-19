c708efdfb92bb397729fcc8c40223dd7d8097ae2
jernchr11 <jernchr@berkeley.edu>
2014-04-18 17:27:57 -0700

    ...

--- HTTPHandler.js
+++ HTTPHandler.js
@@ -174,7 +174,7 @@ app.post('/TESTAPI/resetFixture', function(req, res) {
   //var params = { host: 'ec2-54-197-238-8.compute-1.amazonaws.com',user: 'zbbaxdqhmzxnwh',password: '8WEQZA6SCS4P911KYoKY0lNvpO',database: 'de0l8cfdtcishp',ssl: true };
     ////pg.connect(process.env.DATABASE_URL, function(err, client, done) {
 
-    var client = pg. new pg.Client(process.env.DATABASE_URL);
+    var client = new pg.Client(process.env.DATABASE_URL);
     client.connect();
     client.query('DELETE from ingredients', function(err, result) {
 	client.query('DELETE from history', function(err, result) {
